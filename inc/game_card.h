ff45358310faf819e7e307347ac660efacd2f0c0
Nem-mod <67012818+Nem-mod@users.noreply.github.com>
2022-09-28 15:13:27 +0300

    mr

--- inc/game_card.h
+++ inc/game_card.h
@@ -1,8 +1,23 @@
 #ifndef GAME_CARD_H
 #define GAME_CARD_H
 
+#include "window_sdl.h"
+typedef enum e_card_type {
+    DMG,
+    ARM,
+    MG
+}   t_card_type;
 typedef struct s_game_card {
+    int value;
+    int cost;
+    t_card_type type;
+    char* img_path;
+    SDL_Rect rect;
+    SDL_Texture *tex;
+
 } t_game_card;
 
+t_game_card* mx_create_card(SDL_Window *win, SDL_Renderer *rend, t_card_type type);
 
+void mx_clear_card(t_game_card *card);
 #endif
---
+++ src/game_card.c
@@ -0,0 +1,32 @@
+#include "../inc/game_card.h"
+
+t_game_card* mx_create_card(SDL_Window *win, SDL_Renderer *rend, t_card_type type){
+    t_game_card* card = malloc(sizeof(*card));
+
+    card->cost = rand() % 2 + 1;
+    card->value = rand() % 4 + 3;
+    switch (type)
+    {
+    case DMG:
+        card->img_path = "resource/img/sword-card.png";
+        break;
+    
+    case ARM:
+        card->img_path = "resource/img/shield-card.png";
+        break;
+    case MG:
+        break;
+    }
+    card->rect.h = 150;
+    card->rect.w = 100;
+    card->rect.x = 0;
+    card->rect.y = 0;
+    card->tex = mx_init_texture(card->img_path, win, rend);
+    return card;
+}
+
+void mx_clear_card(t_game_card *card){
+    SDL_DestroyTexture(card->tex);
+    free(card);
+    card = NULL;
+}
