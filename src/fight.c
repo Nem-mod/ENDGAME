83fd3a3cd20667615474624e9fbb491a2b5dc872
Nem-mod <67012818+Nem-mod@users.noreply.github.com>
2022-09-28 21:11:29 +0300

    feat: continue button

--- src/fight.c
+++ src/fight.c
@@ -1,6 +1,7 @@
 #include "../inc/fight.h"
 
 t_fightground *mx_create_fightground(SDL_Window *win, SDL_Renderer *rend, t_character* player) {
+    
     t_fightground *fg = malloc(sizeof(*fg));
 
     fg->background_path = "resource/img/fight/background.jfif";
@@ -37,13 +38,25 @@ t_fightground *mx_create_fightground(SDL_Window *win, SDL_Renderer *rend, t_char
     fg->enemy_rect.y = WINDOW_HEIGHT / 1.5 - 200;
     char* enemy_img = "resource/img/fight/enemy.png";
     fg->enemy = mx_create_character(enemy_img, 20, 1, 1, 1, 1, 1, win, rend);
+    mx_set_enemy(fg->enemy);
 
     fg->cards_rect.h = 150;
     fg->cards_rect.w = 450;
     fg->cards_rect.x = (WINDOW_WIDTH - 450) / 2;
     fg->cards_rect.y = (WINDOW_HEIGHT - 150);
-   
+
+
+    char* button = "resource/img/button-exit.png";
+    fg->button_rect.h = 50;
+    fg->button_rect.w = 200;
+    fg->button_rect.x = (WINDOW_WIDTH - 400);
+    fg->button_rect.y = (WINDOW_HEIGHT - 100);
+
+    fg->continue_button = mx_create_button(fg->button_rect.w, fg->button_rect.h, fg->button_rect.x, fg->button_rect.y, button);
+    fg->continue_button.tex = mx_init_texture(button, win, rend);
+    
     fg->energy = 5;
+    fg->player_action_av = true;
     
     mx_create_cards(win, rend, fg);
     return fg;
@@ -58,7 +71,28 @@ void mx_create_cards(SDL_Window *win, SDL_Renderer *rend, t_fightground* fg) {
     }
 }
 
-void mx_handle_cards(t_fightground *fg) {
+void mx_render_fightground(t_fightground *fg, SDL_Renderer *rend) {
+    SDL_RenderCopy(rend, fg->backg_texture, NULL, &fg->backg_rect);
+
+    SDL_RenderCopy(rend, fg->floor_texture, NULL, &fg->floor_rect);
+    SDL_RenderCopy(rend, fg->frontg_texture, NULL, &fg->frontg_rect);
+    for (int i = 0; i < 3; i++) {
+        SDL_RenderCopy(rend, fg->cards[i]->tex, NULL, &fg->cards[i]->rect);
+    }
+    mx_render_character(fg->player, rend, fg->player_rect);
+    mx_render_character(fg->enemy, rend, fg->enemy_rect);
+    mx_fight(rend, fg);
+}
+
+void mx_clear_fightground(t_fightground *fg) {
+    SDL_DestroyTexture(fg->backg_texture);
+    SDL_DestroyTexture(fg->floor_texture);
+    SDL_DestroyTexture(fg->frontg_texture);
+    free(fg);
+    fg = NULL;
+}
+
+void mx_handle_cards(t_fightground *fg) { // Переделать на перетаскивания
     for (int i = 0; i < 3; i++) {
         if (mx_handle_button(fg->cards[i]->rect)) {
             if (fg->cards[i]->is_active == false) {
@@ -68,26 +102,26 @@ void mx_handle_cards(t_fightground *fg) {
             else if (fg->cards[i]->is_active == true){
                 fg->cards[i]->is_active = false;
                 fg->cards[i]->rect.y += 10;
-            }   
+            }
         }
     }
 }
 
-void mx_render_fightground(t_fightground *fg, SDL_Renderer *rend) {
-    SDL_RenderCopy(rend, fg->backg_texture, NULL, &fg->backg_rect);
-    SDL_RenderCopy(rend, fg->floor_texture, NULL, &fg->floor_rect);
-    SDL_RenderCopy(rend, fg->frontg_texture, NULL, &fg->frontg_rect);
-    for (int i = 0; i < 3; i++) {
-        SDL_RenderCopy(rend, fg->cards[i]->tex, NULL, &fg->cards[i]->rect);
+void mx_fight(SDL_Renderer *rend, t_fightground *fg){
+    while (fg->enemy->current_hp > 0 || fg->player->current_hp > 0) {
+        fg->energy = 5;
+        if (fg->player_action_av)
+        {
+            SDL_RenderCopy(rend, fg->continue_button.tex, NULL, &fg->continue_button.d_rect);
+            if(fg->energy <= 0){
+                fg->player_action_av = false;
+            }
+            if(mx_handle_button(fg->button_rect)) {
+                fg->player_action_av = false;
+            }
+            
+        }
+        break;
     }
-    mx_render_character(fg->player, rend, fg->player_rect);
-     mx_render_character(fg->enemy, rend, fg->enemy_rect);
-}
-
-void mx_clear_fightground(t_fightground *fg) {
-    SDL_DestroyTexture(fg->backg_texture);
-    SDL_DestroyTexture(fg->floor_texture);
-    SDL_DestroyTexture(fg->frontg_texture);
-    free(fg);
-    fg = NULL;
+    
 }
