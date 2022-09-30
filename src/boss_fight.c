edc49a8a3ef96dfb4942bd57714ae11c3e4df013
Nem-mod <67012818+Nem-mod@users.noreply.github.com>
2022-09-30 04:19:24 +0300

    feat: boss fight

--- src/boss_fight.c
+++ src/boss_fight.c
@@ -1,32 +1,83 @@
 #include "../inc/boss_fight.h"
 
 t_fightground *mx_create_bossroom(SDL_Window *win, SDL_Renderer *rend, t_character* player, t_inventory *inv) {
-    t_fightground *boss_room = mx_create_fightground(win, rend, player, inv);
+    
+    t_fightground *bossground = malloc(sizeof(*bossground));
+
+    bossground->background_path = "resource/img/fight/background.jfif";
+    bossground->floor_path = "resource/img/fight/floor.jpg";
+    bossground->frontground_path = "resource/img/fight/column.png";
+
+    bossground->backg_rect.x = 0;
+    bossground->backg_rect.y = 0;
+    bossground->backg_rect.h = WINDOW_HEIGHT / 1.5;
+    bossground->backg_rect.w = WINDOW_WIDTH;
+    bossground->backg_texture = mx_init_texture(bossground->background_path, win, rend);
+
+    bossground->floor_rect.x = 0;
+    bossground->floor_rect.y = WINDOW_HEIGHT / 1.5;
+    bossground->floor_rect.h = WINDOW_HEIGHT / 4;
+    bossground->floor_rect.w = WINDOW_WIDTH;
+    bossground->floor_texture = mx_init_texture(bossground->floor_path, win, rend);
+
+    bossground->frontg_rect.x = 0;
+    bossground->frontg_rect.y = 0;
+    bossground->frontg_rect.h = WINDOW_HEIGHT / 1.5;
+    bossground->frontg_rect.w = WINDOW_WIDTH;
+    bossground->frontg_texture = mx_init_texture(bossground->frontground_path, win, rend);
+
+    bossground->player_rect.h = 300;
+    bossground->player_rect.w = 300;
+    bossground->player_rect.x = 150;
+    bossground->player_rect.y = WINDOW_HEIGHT / 1.5 - 200;
+    bossground->player = player;
+    
+
+    
+    bossground->enemy_rect.h = 300;
+    bossground->enemy_rect.w = 300;
+    bossground->enemy_rect.x = 750;
+    bossground->enemy_rect.y = WINDOW_HEIGHT / 1.5 - 200;
+    bossground->enemy = mx_create_character("resource/img/Boss.png", 150, 35, 10, 15, 10, 50, win, rend);
 
 
-    mx_clear_character(boss_room->enemy);
-    boss_room->enemy = mx_create_character("resource/img/Boss.png",
-     150, 25, 15, 40, 10, 0, win, rend);
-    printf("After creating BOSS\n");
+    bossground->player->healthbar = mx_create_bar(win, rend, HEALTH, &bossground->player_rect);
+    bossground->enemy->healthbar = mx_create_bar(win, rend, HEALTH, &bossground->enemy_rect);
+    bossground->player->shieldbar = mx_create_bar(win, rend, SHIELD, &bossground->player_rect);
+    bossground->enemy->shieldbar = mx_create_bar(win, rend, SHIELD, &bossground->enemy_rect);
 
-    return boss_room;
+    bossground->cards_rect.h = 150;
+    bossground->cards_rect.w = 450;
+    bossground->cards_rect.x = (WINDOW_WIDTH - AMOUNT_OF_CARDS * 150) / 2;
+    bossground->cards_rect.y = (WINDOW_HEIGHT - 150);
+    bossground->discard_cards_count = 0;
+
+    char* button = "resource/img/button-finish.png";
+    bossground->button_rect.h = 50;
+    bossground->button_rect.w = 200;
+    bossground->button_rect.x = (WINDOW_WIDTH - 200) / 2;
+    bossground->button_rect.y = (WINDOW_HEIGHT - 75) / 2;
+
+    bossground->continue_button = mx_create_button(bossground->button_rect.w, bossground->button_rect.h, bossground->button_rect.x, bossground->button_rect.y, button);
+    bossground->continue_button.tex = mx_init_texture(button, win, rend);
+    
+    bossground->energy = AMOUNT_OF_ENERGY;
+    bossground->player_action_av = true;
+    mx_create_cards(win, rend, bossground, inv);
+    bossground->exit_flag = false;
+    return bossground;
 }
 
 int mx_render_bossroom(SDL_Window *win, SDL_Renderer *rend, t_fightground* bossroom, t_inventory *inv) {
     mx_update_fight_bars(bossroom);
-    printf("After updating fightbars\n");
     SDL_RenderCopy(rend, bossroom->backg_texture, NULL, &bossroom->backg_rect);
 
     SDL_RenderCopy(rend, bossroom->floor_texture, NULL, &bossroom->floor_rect);
     SDL_RenderCopy(rend, bossroom->frontg_texture, NULL, &bossroom->frontg_rect);
     mx_render_character(bossroom->player, rend, bossroom->player_rect);
-    printf("Before render copy\n");
     SDL_RenderCopy(rend, bossroom->enemy->character_texture, NULL, &bossroom->enemy_rect);
-    printf("Before render hp bar\n");
     mx_render_bar(bossroom->enemy->healthbar, rend);
-    printf("Before render shield bar\n");
-    // mx_render_bar(bossroom->enemy->shieldbar, rend);
-    printf("After rendering boss\n");
+    mx_render_bar(bossroom->enemy->shieldbar, rend);
     for (int i = 0; i < AMOUNT_OF_CARDS; i++) {
         if (bossroom->cards[i] != NULL && bossroom->cards[i]->is_active == false)
             SDL_RenderCopy(rend, bossroom->cards[i]->tex, NULL, &bossroom->cards[i]->rect);
